3344b83fa02f2e7acffc2bdbf5299702fa7f7205
saddem123 <saddemyassin@gmail.com>
2021-01-08 16:41:56 +0100

    feature: add to cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import Products from "./Products";
 import Header from "./Header";
 import Footer from "./Footer";
 import { Routes, Route } from "react-router-dom";
 import Detail from "./Detail";
-import Cart from "./Card";
+import Cart from "./Cart";
 
 
 export default function App() {
+    const [cart,setCart] = useState([]);
+
+    function addToCart(id, sku){
+        setCart((items) => {
+            const itemInCart = items.find((item) => item.sku == sku);
+            if(itemInCart){
+                // Return a new array with the matching item replaced
+                return items.map((i) => i.sku === sku ? {...i,quantity: i.quantity + 1 }: i);
+            }else {
+                return [...cart,{id,sku,quantity: 1}];
+            }
+        });
+    }
 
     return (
       <>
@@ -18,8 +31,8 @@ export default function App() {
               <Routes>
                   <Route path="/" element={<h1>Welcome to the Carved Rock Fitness</h1>}/>
                   <Route path="/:category" element={<Products/>}/>
-                  <Route path="/:category/:id" element={<Detail/>}/>
-                  <Route path="/cart" element={<Cart/>}/>
+                  <Route path="/:category/:id" element={<Detail addToCart={addToCart}/>}/>
+                  <Route path="/cart" element={<Cart cart={cart}/>}/>
               </Routes>
           </main>
         </div>
--- src/Detail.jsx
+++ src/Detail.jsx
@@ -4,7 +4,7 @@ import useFetch from "./services/useFetch";
 import Spinner from "./Spinner";
 import PageNotFound from "./PageNotFound";
 
-export default function Detail(){
+export default function Detail(props){
     const { id } = useParams();
     const navigate = useNavigate();
     const { data: product, loading, error } = useFetch(`products/${id}`);
@@ -33,7 +33,13 @@ export default function Detail(){
                 }
             </select>
             <p>
-                <button className="btn btn-primary" onClick={() => navigate('/cart')}>
+                <button
+                    disabled={!sku}
+                    className="btn btn-primary"
+                    onClick={() => {
+                        props.addToCart(id,sku);
+                        navigate('/cart')
+                    }}>
                     Add to cart
                 </button>
             </p>
